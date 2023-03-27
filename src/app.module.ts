56b52d7743d376705f0c17dd9b4dda30680865a0
Vera Lavrentyeva <55912590+TwilightHunt@users.noreply.github.com>
2023-03-27 12:24:51 +0300

    build: connect to mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
     controllers: [AppController],
-    providers: [AppService]
+    providers: [AppService],
+    imports: [
+        ConfigModule.forRoot(),
+        MongooseModule.forRoot(process.env.DB_URL, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+      })
+    ],
 })
 export class AppModule {
 
